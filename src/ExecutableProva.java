2418315194a94d99f5496a36cf3f05bdb30fc025
Miquel <miguel.beltran@cide.es>
2020-11-02 19:56:57 +0100

    Version 1 del programa. Se ha añadido el print por pantalla.

---
+++ src/ExecutableProva.java
@@ -0,0 +1,12 @@
+public class ExecutableProva {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        if (0==0){
+            System.out.println("0 es igual a 0");
+        } else{
+            System.out.println("No son iguals");
+        }
+    }
+
+}
